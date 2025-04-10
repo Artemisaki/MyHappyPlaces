b4d95f9aa2f6ee33d1b2e84905b4d85fe42644ef
artemisia <artemisia.margaritoulia@gmail.com>
2025-04-10 13:31:33 +0300

    Detail Activity

---
+++ app/src/main/java/com/example/myhappyplaces/activities/HappyPlaceDetailActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myhappyplaces.activities
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.myhappyplaces.R
+
+
+class HappyPlaceDetailActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //This call the parent constructor
+        super.onCreate(savedInstanceState)
+        // This is used to align the xml view to this class
+        setContentView(R.layout.activity_happy_place_detail)
+    }
+}
